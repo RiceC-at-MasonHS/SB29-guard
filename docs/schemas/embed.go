2bd062eaf02dd35a03af21af2ac0d243029e94a1
Corey Rice <171101634+RiceC-at-MasonHS@users.noreply.github.com>
2025-08-09 22:36:28 -0400

    remove vestigial pieces

--- docs/schemas/embed.go
+++
@@ -1,6 +0,0 @@
-//go:build ignore
-// +build ignore
-
-// This file is intentionally ignored during builds and tests.
-// It exists to keep the docs/schemas folder structured for editors.
-package schemas
